1feed0fc367b8dd8887b861e08053e67e030aa97
tyhhytfirebird9 <alexander.foreman@ucdenver.edu>
2019-08-14 01:20:18 -0600

    Add project files.

---
+++ RazorPagesMovie/Pages/Movie/Create.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesMovie.Pages.Movie
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazorPagesMovie.Models.RazorPagesMovieContext _context;
+
+        public CreateModel(RazorPagesMovie.Models.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public RazorPagesMovie.Models.Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
